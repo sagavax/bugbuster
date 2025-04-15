cfa5b669eb11ba3812c365ea1ab968730355d4fd
Tomas Misura <tmisura@gmail.com>
2025-04-15 11:08:50 +0200

    hotfix

--- bugs_filter_by_priority.php
+++ bugs_filter_by_priority.php
@@ -25,6 +25,7 @@ while ($row = mysqli_fetch_array($result)) {
       $is_fixed = (int) ($row['is_fixed'] ?? 0);
       $added_date = htmlspecialchars($row['added_date'] ?? '', ENT_QUOTES, 'UTF-8');
       $application = htmlspecialchars($row['bug_application'] ?? '', ENT_QUOTES, 'UTF-8');
+      $bug_github_url = htmlspecialchars($row['bug_github_url'] ?? '', ENT_QUOTES, 'UTF-8');
 
   
       // Počet komentárov
@@ -34,9 +35,7 @@ while ($row = mysqli_fetch_array($result)) {
       $add_comment = "<button type='button' name='add_comment' class='button small_button' onclick='addNewComment();')><i class='fa fa-comment'></i></button>";
       $fixed_label = $is_fixed ? "<div class='span_fixed'>fixed</div>" : "";
       $action_buttons = $is_fixed ? 
-          "<button type='button' name='see_bug_details' class='button small_button'><i class='fa fa-eye'></i></button>
-           <button type='button' name='bug_remove' class='button small_button'><i class='fa fa-times'></i></button>
-           {$add_comment}" : // Pridanie komentára aj pre fixed stav
+          "<button type='button' name='see_bug_details' class='button small_button'><i class='fa fa-eye'></i></button>" : // Pridanie komentára aj pre fixed stav
           "<button type='button' name='see_bug_details' class='button small_button'><i class='fa fa-eye'></i></button>
            <button type='button' name='to_fixed' class='button small_button'><i class='fa fa-check'></i></button>
            <button type='button' name='bug_remove' class='button small_button'><i class='fa fa-times'></i></button>
@@ -46,17 +45,24 @@ while ($row = mysqli_fetch_array($result)) {
       // Generovanie HTML výstupu
      
       echo "<div class='bug' bug-id='{$bug_id}'>
-      <div class='bug_title'>{$bug_title} {$fixed_label}</div>
-      <div class='bug_text'>{$bug_text}</div>
-      <div class='bug_footer'>
-          <div class='bug_application'>{$application}</div>                         
-          <div class='bug_status {$bug_status}'>{$bug_status}</div>
-          <div class='bug_priority {$bug_priority}'>{$bug_priority}</div>
-          <div class='bug_comments'>{$nr_of_comments} comments</div>
-          <div class='bug_action'>
-                {$action_buttons}
-          </div>
-      </div>
-  </div>";
+            <div class='bug_title'>$bug_title</div>";
+       echo "<div class='bug_text'>$bug_text</div>
+        <div class='bug_footer'>
+            <div class='bug_github_link' data-link='$bug_github_url'><i class='fab fa-github-alt'></i></div>
+            <div class='bug_application'>$application</div>                         
+            <div class='bug_status $bug_status'>$bug_status</div>
+            <div class='bug_priority $bug_priority'>$bug_priority</div>
+            <div class='bug_comments'>$nr_of_comments comments</div>";
+
+
+             if ($is_fixed == 1) {
+                  echo "<div class='bug_fixed'>fixed</div>";                                  }  
+
+         echo" <div class='bug_action'>
+                  {$action_buttons}
+            </div>
+        </div>
+    </div>";
+
   
   }
