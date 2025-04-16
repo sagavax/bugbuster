df58d278e9f95a3729cce9e20104e31af89a9a41
Tomas Misura <tmisura@gmail.com>
2025-04-16 07:37:31 +0200

    new parsonal access token

--- bugs_create_new.php
+++ bugs_create_new.php
@@ -33,7 +33,7 @@
     mysqli_query($link, $save_bug) or die("MySQLi ERROR: ".mysqli_error($link));
   
     //add to github
-    $token = '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';
+    $token = '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';
     $result = createGithubIssue($bug_title, $bug_text, $token);
 
     
