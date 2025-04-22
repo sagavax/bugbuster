da526cbd0c52c24cf8dbda05bd91fd6965234537
Tomas Misura <tmisura@gmail.com>
2025-04-22 16:33:25 +0200

    diary record sorder by application

---
+++ apps/application_diary.php
@@ -0,0 +1,21 @@
+<?php 
+
+    include ("../includes/dbconnect.php");
+    include ("../includes/functions.php");
+
+
+    $get_diary = "SELECT * from apps_diary WHERE app_id=$app_id ORDER BY id DESC";
+    $result = mysqli_query($link, $get_diary) or die(mysqli_error($link));
+    while ($row = mysqli_fetch_array($result)) {
+        $diary_date = $row['created_date'];
+        $diary_text = $row['diary_text'];
+        $app_id = $row['project_id'];
+        $diary_app = GetAppName($app_id);
+
+        echo "<div class='diary_record' data-id='" . $row['id'] . "'>
+                <div class='diary_date'><span class='date_time'>$diary_date</span></div>
+                <div class='diary_text'>$diary_text</div>
+                <div class='diary_app'><span class='app_name'>$diary_app<span></div>
+                <div class='diary_action'><button class='button button_small' name='delete'><i class='fa fa-times'></i></button></div>
+              </div>";
+    }
