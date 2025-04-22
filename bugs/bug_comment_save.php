1c1c9509cee5807584b15150a0041adc7b4e29a6
Tomas Misura <tmisura@gmail.com>
2025-04-22 17:39:44 +0200

    hotfix

--- bugs/bug_comment_save.php
+++ bugs/bug_comment_save.php
@@ -12,11 +12,11 @@ $save_comment = "INSERT into bugs_comments (bug_id,bug_comm_header, bug_comment,
  $result=mysqli_query($link, $save_comment) or die("MySQLi ERROR: ".mysqli_error($link));
 
 //app log
-$diary_text="Minecraft IS: Bolo pridane novy kommentar k bugu id <b>$bug_id</b>";
+$diary_text="Bol pridany novy kommentar k bugu id <b>$bug_id</b>";
 $sql="INSERT INTO app_log (diary_text, date_added) VALUES ('$diary_text',now())";
 $result = mysqli_query($link, $sql) or die("MySQLi ERROR: ".mysqli_error($link));
 
 //add to timeline
 $diary_text="Bol pridany novy kommentar";
-$create_record="INSERT INTO bug_timeline (object_id, object_type, parent_object_id, timeline_text, created_date) VALUES ($bug_id, 0,'bug_comment','$diary_text', now())";
+$create_record="INSERT INTO bug_timeline (object_id, object_type, parent_object_id, timeline_text, created_date) VALUES ($bug_id, 'bug', $bug_id,'$diary_text', now())";
 $result = mysqli_query($link, $create_record) or die("MySQLi ERROR: ".mysqli_error($link));
