08c4268d0eeddfab45199616c4fd77244e7dff94
Tomas Misura <tmisura@gmail.com>
2025-04-23 21:29:37 +0200

    removed whitespaces

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,9 +1,6 @@
 <?php
   date_default_timezone_set('Europe/Bratislava');
-
-
-
- $link = mysqli_connect("localhost", "root", "", "bugbuster", 3306);
+  $link = mysqli_connect("localhost", "root", "", "bugbuster", 3306);
 
   if (!$link) {
       echo "Error: Unable to connect to MySQL." . PHP_EOL;
