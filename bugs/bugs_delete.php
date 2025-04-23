58d9d9f91cb5f1802f8bc8fb03e742fe5148ca20
Tomas Misura <tmisura@gmail.com>
2025-04-23 13:55:04 +0200

    bug soft delete

--- bugs/bugs_delete.php
+++ bugs/bugs_delete.php
@@ -9,22 +9,15 @@ if(isset($_POST['bug_id'])){
 }
 
 
-$remove_bug = "DELETE from bugs WHERE bug_id=$bug_id";
+//$remove_bug = "DELETE from bugs WHERE bug_id=$bug_id";
+$remove_bug = "UPDATE bugs SET bug_status='deleted' WHERE bug_id=$bug_id";
 $result = mysqli_query($link, $remove_bug) or die("MySQLi ERROR: ".mysqli_error($link));
 
-$remove_comments = "DELETE from bugs_comments WHERE bug_id=$bug_id";
-$result = mysqli_query($link, $remove_comments) or die("MySQLi ERROR: ".mysqli_error($link));
-
-$diary_text="Bug s id $bug_id bola vymazany";
+$diary_text = mysqli_real_escape_string($link, "Bug s id $bug_id bol vymazany");
 $create_record="INSERT INTO app_log (diary_text, date_added) VALUES ('$diary_text', now())";
 $result = mysqli_query($link, $create_record) or die("MySQLi ERROR: ".mysqli_error($link));
 
 //add to timeline
-$diary_text="Bug s id $bug_id bola vymazany";
-$create_record="INSERT INTO bug_timeline (object_id, object_type, parent_object_id, timeline_text, created_date) VALUES ($bug_id, 'bug', 0,'$diary_text', now())";
-$result = mysqli_query($link, $create_record) or die("MySQLi ERROR: ".mysqli_error($link));
-
-//add to timeline
-$diary_text="Bug bola vymazany";
+$diary_text=mysqli_real_escape_string($link,"Bug bol vymazany");
 $create_record="INSERT INTO bugs_timeline (object_id, object_type, parent_object_id, timeline_text, created_date) VALUES ($bug_id, 'bug', $bug_id,'$diary_text', now())";
 $result = mysqli_query($link, $create_record) or die("MySQLi ERROR: ".mysqli_error($link));
