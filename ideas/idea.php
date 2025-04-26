ddf34021c3b44400e9b4276c41b07638f8c09be5
Tomas Misura <tmisura@gmail.com>
2025-04-26 11:28:54 +0200

    added more information for idea

--- ideas/idea.php
+++ ideas/idea.php
@@ -35,11 +35,14 @@ session_start();
                 $get_idea = "SELECT * FROM ideas WHERE idea_id = $idea_id";
                 $result = mysqli_query($link, $get_idea);
                 while ($row = mysqli_fetch_array($result)) {
-                    $idea_id = $row['idea_id'];
-                    $idea_title = $row['idea_title'];
-                    $idea_text = $row['idea_text'];
-                    $added_date = $row['added_date'];
-                    $is_implemented = $row['is_implemented'];
+                    $idea_id = htmlspecialchars($row['idea_id'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $idea_title = htmlspecialchars($row['idea_title'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $idea_text = htmlspecialchars($row['idea_text'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $idea_priority = htmlspecialchars($row['idea_priority'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $idea_status = htmlspecialchars($row['idea_status'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $is_implemented = htmlspecialchars($row['is_implemented'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $added_date = htmlspecialchars($row['added_date'] ?? '', ENT_QUOTES, 'UTF-8');
+                    $application = htmlspecialchars($row['idea_application'] ?? '', ENT_QUOTES, 'UTF-8');
 
                     echo "<div class='idea_detail'>";
                     if (isset($idea_title)) {
@@ -48,15 +51,21 @@ session_start();
 
                     echo "<div class='idea_text'>$idea_text</div>";
                     echo "<div class='idea_footer'>";
-                    if ($is_implemented == 0) {
-                        echo "<form action='' method='post'>";
-                        echo "<input type='hidden' name='idea_id' value='$idea_id'>";
-                        echo "<button type='submit' name='to_apply' class='button small_button'><i class='fa fa-check'></i></button>";
-                        echo "</form>";
-                    } elseif ($is_implemented == 1) {
-                        echo "<div class='idea_implemented'>Implemented</div>";
-                    }
-                    echo "</div>"; // idea_footer
+                        echo "<div class='idea_application'>{$application}</div>";
+                        $nr_of_comments = GetCountIdeaComments($idea_id);
+                        echo "<div class='nr_of_comments'>$nr_of_comments comment(s)</div>";
+                        echo "<div class='idea_status'>$idea_status</div><div class='idea_priority $idea_priority'>$idea_priority</div>";
+                        echo "<button type='button' name='see_idea_details' class='button small_button'><i class='fa fa-eye'></i></button>";
+
+                        if ($is_implemented == 0) {
+                            echo "<form action='' method='post'>";
+                            echo "<input type='hidden' name='idea_id' value='$idea_id'>";
+                            echo "<button type='submit' name='to_apply' class='button small_button'><i class='fa fa-check'></i></button>";
+                            echo "</form>";
+                        } elseif ($is_implemented == 1) {
+                            echo "<div class='idea_implemented'>Implemented</div>";
+                        }
+                        echo "</div>"; // idea_footer
                     echo "</div>"; // idea
                 }      
                 ?>
@@ -98,6 +107,7 @@ session_start();
                     <input type="text" name="idea_comment_header" autocomplete="off" placeholder="Type title here">
                     <textarea name="idea_comment" placeholder="Type comment here..."></textarea>
                     <div class="idea_comment_action">
+                        
                         <?php
                         if ($is_implemented == 0) {
                             echo "<button name='save_idea_comment' class='button small_button' type='button'>Save</button>";
